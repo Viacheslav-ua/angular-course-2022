324057f475f9d6a94a52613d2a9a6e9ecc0490e0
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2022-07-25 17:35:11 +0300

    part2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
-import { products as data } from './data/products';
+import { ProductService } from './services/products.service';
+// import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  
   title = 'angular course 2022'
 
-  products = data
+  // products = data
+  products: IProduct[] = []
+
+  constructor(private productsService: ProductService) {}
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.productsService.getAll().subscribe((products) => {
+      // console.log(products);
+      this.products = products
+    })
+  }
 }
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http"
+import { Observable } from "rxjs";
+import { IProduct } from "../models/product";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+      //    params: new HttpParams().append('limit',5)
+      params: new HttpParams({
+        // fromString: 'limit=6'
+        fromObject: {limit: 7}
+      })
+    })
+  }
+}
